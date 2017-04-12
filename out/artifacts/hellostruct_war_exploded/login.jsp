9d495819be667c6d636827a9564352b8974721ea
aweng126 <13075313106@163.com>
2017-04-12 21:24:47 +0800

    环境部署成功，可以成功访问

---
+++ out/artifacts/hellostruct_war_exploded/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kingwen
+  Date: 2017/4/12
+  Time: 20:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+    <h1>登录界面</h1>
+  </body>
+</html>
---
+++ src/action/LoginAction.java
@@ -0,0 +1,31 @@
+package action;
+
+/**
+ * Created by kingwen on 2017/4/12.
+ */
+public class LoginAction {
+    public String username;
+    public String userpwd;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpwd() {
+        return userpwd;
+    }
+
+    public void setUserpwd(String userpwd) {
+        this.userpwd = userpwd;
+    }
+
+    public String execute(){
+        return "success";
+    }
+
+
+}
